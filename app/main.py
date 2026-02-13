b7d9d1b2b2fc189d76d66a7464937183b5fc6674
harivanshgiri968-max <harivanshgiri968@gmail.com>
2026-02-14 04:16:25 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from app.services import analyze_website
 
 app = FastAPI(title="Professional SEO API")
 
+@app.get("/")
+def home():
+    return {"message": "SEO API is Live"}
+
 @app.get("/analyze")
 def analyze(url: str, api_key: str = Depends(verify_api_key)):
     return analyze_website(url)
